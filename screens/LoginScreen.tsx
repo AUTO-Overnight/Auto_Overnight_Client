da610714e865f507fd8d17a05177e82d97821164
TMInstaller <ajvldmlqjqclr@naver.com>
2023-12-16 13:25:27 +0900

    rename: 디렉토리 변경

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -4,7 +4,7 @@ import { Text, View, Image } from "react-native";
 import { Button } from "react-native-paper";
 import { StyleSheet } from "react-native";
 import { SCREEN_WIDTH } from "../constants/style";
-import LoginInput from '../src/components/login/LoginInput';
+import LoginInput from "../components/login/LoginInput";
 import { useState } from "react";
 
 type LoginScreenNavigationProp = StackNavigationProp<
@@ -25,18 +25,19 @@ const LoginScreen: React.FC<LoginProps> = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text>
-          로그인
-        </Text>
+        <Text>로그인</Text>
       </View>
       <View style={styles.body}>
-        <LoginInput inputType="ID" />
-        <LoginInput inputType="PW" />
-        
-        <Button mode="contained" style={styles.buttonView} buttonColor="#1F2937" loading={isPressed} onPress = {() => isPressedBtn()}>
-          <Text>
-            로그인
-          </Text>
+        <LoginInput inputType='ID' />
+        <LoginInput inputType='PW' />
+
+        <Button
+          mode='contained'
+          style={styles.buttonView}
+          buttonColor='#1F2937'
+          loading={isPressed}
+          onPress={() => isPressedBtn()}>
+          <Text>로그인</Text>
         </Button>
       </View>
     </View>
@@ -66,6 +67,6 @@ const styles = StyleSheet.create({
   buttonView: {
     marginHorizontal: 20,
     marginVertical: 20,
-    borderRadius: 5
-  }
+    borderRadius: 5,
+  },
 });
