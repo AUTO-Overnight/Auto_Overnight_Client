784f033e04ac596f1c461475bf9d9be38872a76d
devGEP <ju0032564@gmail.com>
2024-01-07 05:06:55 +0900

    refact: 로그인 화면 색상 상수화

--- constants/style.ts
+++ constants/style.ts
@@ -2,8 +2,14 @@ import { Dimensions } from "react-native";
 
 export const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
+export const LOGIN_COLORS = {
+  background: "#fff",
+  loginButton: "#1F2937",
+  loginInfo: "#7b7b7b",
+}
+
 export const BONUS_POINT_COLORS = {
   background: "#fff",
   header: "#f6f8ff",
   penalty: "#fff5f5",
-}
+};
--- src/user/ui/LoginInput.tsx
+++ src/user/ui/LoginInput.tsx
@@ -1,4 +1,6 @@
 import { TextInput } from "react-native-paper";
+import { LOGIN_COLORS } from "../../../constants/style";
+import { StyleSheet } from "react-native";
 
 interface LoginInputProps {
   inputType: "ID" | "PW";
@@ -20,10 +22,17 @@ export default function LoginInput({
       value={value}
       placeholder={inputType}
       onChangeText={onChangeText}
-      activeOutlineColor="#1F2937"
+      activeOutlineColor={LOGIN_COLORS.loginButton}
       secureTextEntry={isPassword}
-      style={{ marginVertical: 3, marginHorizontal: 20 }}
+      style={styles.textInput}
       left={<TextInput.Icon icon={iconName} />}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  textInput: {
+    marginVertical: 3,
+    marginHorizontal: 20,
+  },
+});
