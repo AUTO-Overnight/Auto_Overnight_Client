58e1dbf85f03e5435fb7471ff1b7ec52e82480a0
TMInstaller <ajvldmlqjqclr@naver.com>
2024-01-09 16:27:56 +0900

    rename: 파일 명 수정

--- store/mode.ts
+++ store/mode.ts
@@ -1,11 +1,11 @@
-import { create } from "zustand";
+import { create } from 'zustand';
 
 type Store = {
   isDarkMode: boolean;
   toggleMode: () => void;
 };
 
-export const useStore = create<Store>((set) => ({
+export const useModeStore = create<Store>((set) => ({
   isDarkMode: false, // 초기 다크모드 설정
   toggleMode: () => set((state) => ({ isDarkMode: !state.isDarkMode })), // 모드 전환 함수
 }));
